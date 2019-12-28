3e8537169e95eb8494619aa5a61ef293b3c102f8
SergeyDorozhko <sergdorozhko@gmail.com>
2019-12-28 07:42:53 +0300

    first spring start

---
+++ springStart/src/test/java/by/dorozhko/springStart/SpringStartApplicationTests.java
@@ -0,0 +1,16 @@
+package by.dorozhko.springStart;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class SpringStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
